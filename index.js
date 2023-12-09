cc9f4dae870c46ceadedf7b455228821fdd49313
MRajaRahmad <rajakecenemen@gmail.com>
2023-12-09 19:50:32 +0700

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_xx7x7s9";
+  const templateID = "template_nmtasaa";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
